7d82c1a4bf50c8f22b6b9c6ebdb769a26fbde3cb
khandakerahsunhabib <ahsunhabib96bd@gmail.com>
2024-11-25 15:32:12 +0600

    scoreboar screen design done

--- lib/scoreboard.dart
+++ lib/scoreboard.dart
@@ -1,16 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Scoreboard extends StatelessWidget {
+class Scoreboard extends StatefulWidget {
   const Scoreboard({super.key});
 
   @override
+  State<Scoreboard> createState() => _ScoreboardState();
+}
+
+class _ScoreboardState extends State<Scoreboard> {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal.shade300,
-        foregroundColor: Colors.white,
-        title: const Text('Live Score'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.teal.shade300,
+          foregroundColor: Colors.white,
+          title: const Text('Live Score'),
+        ),
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Badge(
+                  backgroundColor: _indicatorColor(true),
+                ),
+                title: Text('Match id'),
+                subtitle: Text('Team One: Bangladesh\n Team Two: England'),
+              );
+            }));
+  }
+
+  Color _indicatorColor(bool isMatchRunning) {
+    return isMatchRunning ? Colors.green : Colors.grey;
   }
 }
